5126bb79294728ef90dbbaf11cb7624672991f0c
yagnik5108-netizen <yagnik5108@gmail.com>
2025-08-28 13:11:25 +0530

    Add files via upload

---
+++ swavalu24.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main() {
+    int a, b, temp;
+    printf("Enter two integers: ");
+    scanf("%d %d", &a, &b);
+    temp = a; a = b; b = temp;
+    printf("After swap: a = %d, b = %d\n", a, b);
+    return 0;
+}
